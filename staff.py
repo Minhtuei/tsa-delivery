86caf6c232708a0979048966fb86ad5c53e1f6ee
Minhtuei <tue.tran20032003@gmail.com>
2025-05-15 13:15:24 +0700

    hotfix: get status from user instead of staff

--- staff.py
+++ staff.py
@@ -4,20 +4,28 @@ from sqlalchemy import select
 from sqlmodel import Field, Session, SQLModel
 
 
-class StaffStatus(str, Enum):
+class UserStatus(str, Enum):
     AVAILABLE = "AVAILABLE"
     BUSY = "BUSY"
-    OFFLINE = "OFFLINE"
+
+
+class UserRole(str, Enum):
+    STUDENT = "STUDENT"
+    STAFF = "STAFF"
+    ADMIN = "ADMIN"
 
 
 class Staff(SQLModel, table=True):
-    __tablename__ = "Staff"
-    staffId: str = Field(primary_key=True, default=None)
-    status: StaffStatus
+    __tablename__ = "User"
+    id: str = Field(primary_key=True, default=None)
+    status: UserStatus
+    role: UserRole
 
     @classmethod
     def get_total_available_staff(cls, session: Session) -> int:
         """Retrieve total number of available staff."""
-        statement = select(cls).where(cls.status == StaffStatus.AVAILABLE)
+        statement = select(cls).where(
+            (cls.status == UserStatus.AVAILABLE) & (cls.role == UserRole.STAFF)
+        )
         result = session.exec(statement).all()
         return len(result)
